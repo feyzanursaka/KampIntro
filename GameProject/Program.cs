e43affc7df36325cb7f154387ab20c4bdebdf54f
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-02-22 12:04:13 +0200

    27 Ocak Ders Sonu

---
+++ GameProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1996,
+                FirstName = "FEYZA",
+                LastName = "SAKA",
+                IdentityNumber = 12345
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
