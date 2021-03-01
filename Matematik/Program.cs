3e94b497e02934ae85f2936d56163d7820af612f
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-01 23:50:46 +0200

    20 Ocak Ders Sonu

--- Matematik/Program.cs
+++ Matematik/Program.cs
@@ -7,8 +7,8 @@ namespace Matematik
         static void Main(string[] args)
         {
             DortIslem dortIslem = new DortIslem();
-            dortIslem.Topla(5, 6);
 
+            dortIslem.Topla(5, 6);
             dortIslem.Topla(6, 9);
         }
     }
