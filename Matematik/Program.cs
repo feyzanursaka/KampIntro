1d0d5a94aba3d3bcab40e0b00bb7767f860d260a
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-01 23:49:41 +0200

    20 Ocak Ders Sonu

--- Matematik/Program.cs
+++ Matematik/Program.cs
@@ -6,27 +6,10 @@ namespace Matematik
     {
         static void Main(string[] args)
         {
-            //DortIslem dortIslem = new DortIslem();
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(5, 6);
 
-            //dortIslem.Topla(5, 6);
-            //dortIslem.Topla(6, 9);
-
-            int sayimizOut;
-            DegistirOut(out sayimizOut);
-            Console.WriteLine(sayimizOut);
-
-            int sayimizRef = 0;  // değer verilmediğinde hata verecektir.
-            DegistirRef(ref sayimizRef);
-            Console.WriteLine(sayimizRef);
-        }
-        static void DegistirRef(ref int sayiRef)
-        {
-            sayiRef = 1234;
-        }
-
-        static void DegistirOut(out int sayiOut)
-        {
-            sayiOut = 1234;
+            dortIslem.Topla(6, 9);
         }
     }
 }
