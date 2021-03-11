5aaec12732d3b907a4ed3c6e6a599776717cf5da
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-12 01:53:45 +0200

    Reflection

---
+++ Reflection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortislem= new DortIslem(2,3);
+            //Console.WriteLine(dortislem.Topla2());
+            //Console.WriteLine(dortislem.Topla(3, 4));
+
+            var type = typeof(DortIslem);
+            DortIslem dortIslem = (DortIslem)Activator.CreateInstance(type,6,7);
+            Console.WriteLine(dortislem.Topla(4, 5));
+            Console.WriteLine(dortIslem.Topla2());
+            Console.ReadLine();
+        }
+    }
+    public class DortIslem
+    {
+        private int _sayi1;
+        private int _sayi2;
+        public DortIslem(int sayi1, int sayi2)
+        {
+            _sayi1 = sayi1;
+            _sayi2 = sayi2;
+        }
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;
+        }
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+    }
+}
