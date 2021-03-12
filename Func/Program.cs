4bd46eb6ff471bfb7643b96a521136c3ed6cf72d
feyzanursaka1996@gmail.com <feyzanursaka1996@gmail.com>
2021-03-12 22:43:14 +0200

    Func

---
+++ Func/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = Topla;
+            Console.WriteLine(add(3,5));
+
+            //Console.WriteLine(Topla(2,3));
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };           
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+            Console.WriteLine(getRandomNumber());
+            Console.WriteLine(getRandomNumber2());
+        }
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
